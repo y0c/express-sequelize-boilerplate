65820eee2079106b25ecc107d69bbcbee926b917
LimHoSung <wonder789@naver.com>
2018-04-01 19:16:04 +0900

    sequelize connection test complete

--- src/config/config.js
+++ src/config/config.js
@@ -6,5 +6,12 @@ export default (env) => ({
     request: {
         bodyLimit: env.BODY_LIMIT,
         corsHeader: env.CORS_HEADER
+    },
+    db: {
+        userName: env.DB_USER_NAME,
+        userPassword: env.DB_USER_PASSWORD,
+        database: env.DB_DATABASE,
+        dialect: env.DB_DIALECT,
+        host: env.DB_HOST
     }
 })
--- src/db.js
+++ src/db.js
@@ -1,4 +1,33 @@
+import Sequelize from 'sequelize';
+import config from './config';
+
 export default callback => {
 	// connect to a database if needed, then pass it to `callback`:
+	const sequelize = new Sequelize(
+		config.db.database,
+		config.db.userName,
+		config.db.userPassword, {
+			host: config.db.host,
+			dialect: config.db.dialect,
+			operatorsAliases: false,
+
+			pool: {
+				max: 5,
+				min: 0,
+				acquire: 30000,
+				idle: 10000
+			}
+		});
+
+	// Or you can simply use a connection uri
+
+	sequelize
+		.authenticate()
+		.then(() => {
+			console.log('Connection has been established successfully.');
+		})
+		.catch(err => {
+			console.error('Unable to connect to the database:', err);
+		});
 	callback();
 }
