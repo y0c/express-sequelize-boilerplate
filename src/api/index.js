2844404436bea8741f4d90aba69f0f84f039e5cb
y0c <wonder789@naver.com>
2018-04-29 00:09:07 +0900

    [WIP] jwt check

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,12 @@
 import { version } from '../../package.json';
 import { Router } from 'express';
+import authJwt from 'config/passport/jwt';
 import user from './user';
 
 export default (() => {
 	let api = Router();
 
+	api.use(authJwt.initialize());
 	api.use('/user', user);
 
 	// perhaps expose some API metadata at the root
@@ -13,4 +15,4 @@ export default (() => {
 	});
 
 	return api;
-})()
+})();
